bd5954f02677d9e4224f26255c96a5c372c058b4
emman123 <67055497+mantot-123@users.noreply.github.com>
2025-07-08 13:20:55 +0800

    Added an explosion animation when the player hits a bomb and loses

--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -2,20 +2,20 @@ class_name Bomb
 
 extends Area2D
 
-var currentGame: GameManager
 @onready var hitbox: Hitbox = $Hitbox
 
 signal bombContact
 
 func _ready() -> void:
-	currentGame = self.get_tree().current_scene # Get the currently open "Game" scene
 	body_entered.connect(_onBombContact)
 	hitbox.isHit.connect(_onBombContact)
 
 func _onBombContact(body: Node2D) -> void:
 	if body is Player:
-		currentGame.gameOver.emit() # Sends a "game over" signal to the current game manager
+		body.isLost = true
+		body.currentGame.gameScrollSpeed = 0.0
+		body.animationPlayer.play("defeat")
+		self.queue_free()
 	elif body is Bullet:
 		body.bulletHit.emit()
 		self.queue_free() # Remove the bomb from the scene tree
-
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,25 +4,34 @@ extends CharacterBody2D
 
 const JUMP_VELOCITY = -500.0
 
+@export var currentGame: GameManager
+
 @onready var sprite: AnimatedSprite2D = $Sprite
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 var toggleFlying: bool = false
+var isLost: bool = false
 
 func _physics_process(delta: float) -> void:
-	# Handle jumping and flying
-	
-	if Input.is_action_pressed("jump"):
-		self.velocity = Vector2(0, JUMP_VELOCITY)
-		toggleFlying = true
-	else:
-		self.velocity += 2 * get_gravity() * delta
-		if is_on_floor():
-			toggleFlying = false
+	if not isLost:
+		# Handle jumping and flying
+		if Input.is_action_pressed("jump"):
+			self.velocity = Vector2(0, JUMP_VELOCITY)
+			toggleFlying = true
+		else:
+			self.velocity += 2 * get_gravity() * delta
+			if is_on_floor():
+				toggleFlying = false
 
-	if toggleFlying:
-		sprite.play("flying")
+		if toggleFlying:
+			sprite.play("flying")
+		else:
+			sprite.play("running")
 	else:
-		sprite.play("running")
-
+		self.velocity = Vector2.ZERO
 
 	move_and_slide()
+
+
+func lose():
+	currentGame.gameOver.emit() # Sends a "game over" signal to the current game manager
